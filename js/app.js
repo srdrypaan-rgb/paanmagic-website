aef6c904402e5800202839fd8a7b94fdb069017d
srdrypaan-rgb <srdrypaan@gmail.com>
2026-06-30 12:15:10 +0530

    Mobile Site Corrections

--- js/app.js
+++ js/app.js
@@ -91,3 +91,48 @@ counters.forEach(counter=>{
 });
 
 document.getElementById("year").textContent = new Date().getFullYear();
+
+const sections = document.querySelectorAll("section[id], footer[id]");
+const navLinks = document.querySelectorAll("nav a");
+
+window.addEventListener("scroll", () => {
+
+    let current = "";
+
+    sections.forEach(section => {
+
+        const sectionTop = section.offsetTop - 150;
+        const sectionHeight = section.offsetHeight;
+
+        if (window.scrollY >= sectionTop &&
+            window.scrollY < sectionTop + sectionHeight) {
+
+            current = section.getAttribute("id");
+        }
+
+    });
+
+    navLinks.forEach(link => {
+
+        link.classList.remove("active");
+
+        if (link.getAttribute("href") === "#" + current) {
+            link.classList.add("active");
+
+        }
+
+    });
+
+});
+
+const mobileLinks = document.querySelectorAll("nav a");
+
+mobileLinks.forEach(link => {
+
+    link.addEventListener("click", () => {
+
+        nav.classList.remove("active");
+
+    });
+
+});
